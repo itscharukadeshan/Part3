9954117a81013016f2e053ee5f2517821129a1a5
itscharukadeshan <itscharukadeshan@gmail.com>
2023-05-07 15:46:30 +0530

    3.2: Phonebook backend step2 completed

--- index.js
+++ index.js
@@ -35,6 +35,21 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const now = new Date();
+  const dayOfWeek = now.toLocaleDateString("en-US", { weekday: "long" });
+  const month = now.toLocaleDateString("en-US", { month: "long" });
+  const year = now.getFullYear();
+  const time = now.toLocaleTimeString("en-US");
+  const timeZoneOffset = now.getTimezoneOffset();
+  const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  res.send(
+    ` phone book has info for ${persons.length} people <br />
+    
+      ${dayOfWeek} ${month} ${year} ${time} GMT ${timeZoneOffset} (${timeZoneName})`
+  );
+});
+
 app.listen(port, () => {
   console.log(`express sever is running at ${port}`);
 });
