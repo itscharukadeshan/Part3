9a64a869cf1ff5e2c4a2b22f3175c58a409d1e00
charuka deshan <itscharukadeshan@gmail.com>
2023-05-09 10:36:58 +0530

    add build to the back end and make minor changes

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
+
+app.use(express.static("build"));
 
 const morgan = require("morgan");
 
