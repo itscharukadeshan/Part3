adc71ddf253d8431c23249a490036d4a5c3da2f0
charuka deshan <itscharukadeshan@gmail.com>
2023-05-12 12:43:37 +0530

    add express and port

--- The-Phonebook/Back-end/index.js
+++ The-Phonebook/Back-end/index.js
@@ -1,137 +1,128 @@
 /** @format */
 
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3001;
+const morgan = require("morgan");
 const cors = require("cors");
 
-app.use(cors());
+const Person = require("./models/person");
 
+const app = express();
+app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 
-app.use(express.static("build"));
-
-const morgan = require("morgan");
-
-morgan.token("response-body", (req, res) => {
-  if (req.method === "POST") {
-    return JSON.stringify(req.body);
-  }
-  return "";
-});
-
-app.use((req, res, next) => {
-  morgan("tiny")(req, res, () => {});
-  next();
-});
-
-app.use((req, res, next) => {
-  morgan(":response-body")(req, res, () => {});
-  next();
-});
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("express server up and running");
-});
-
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
-
-app.get("/info", (req, res) => {
-  const now = new Date();
-  const dayOfWeek = now.toLocaleDateString("en-US", { weekday: "long" });
-  const month = now.toLocaleDateString("en-US", { month: "long" });
-  const year = now.getFullYear();
-  const time = now.toLocaleTimeString("en-US");
-  const timeZoneOffset = now.getTimezoneOffset();
-  const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  res.send(
-    ` phone book has info for ${persons.length} people <br />
-    
-      ${dayOfWeek} ${month} ${year} ${time} GMT ${timeZoneOffset} (${timeZoneName})`
-  );
-});
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((persons) => persons.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).json({ error: `person with ${id} not found` });
+morgan.token("postdata", (request) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
   }
 });
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postdata"
+  )
+);
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.get("/info", (request, response, next) => {
+  Person.count({})
+    .then((count) => {
+      const timestamp = new Date();
+      response.send(
+        `<p>Phonebook has info for ${count} people</p>
+      <p>${timestamp}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "name or number missing",
-    });
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+});
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", async (request, response, next) => {
+  if (!request.body.name) {
+    return response.status(400).json({ error: "name missing" });
+  }
+  if (!request.body.number) {
+    return response.status(400).json({ error: "number missing" });
   }
 
-  const nameExists = persons.some((p) => p.name === body.name);
-
-  if (nameExists) {
-    return res.status(400).json({
-      error: "name already exists.name must be unique",
-    });
+  let exists = await Person.exists({ name: request.body.name });
+  if (exists) {
+    return response.status(409).json({ error: "name must be unique" });
   }
 
-  const numberExists = persons.some((p) => p.number === body.number);
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number,
+  });
 
-  if (numberExists) {
-    return res.status(400).json({
-      error: "number already exists.number must be unique",
-    });
-  }
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
 
+app.put("/api/persons/:id", (request, response, next) => {
   const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
+    name: request.body.name,
+    number: request.body.number,
   };
 
-  persons.push(person);
-
-  res.json(person);
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
-};
+function unknownEndpoint(request, response) {
+  response.status(404).send({ error: "unknown endpoint" });
+}
 
-app.listen(PORT, () => {
-  console.log(`express sever is running at ${PORT}`);
-});
+app.use(unknownEndpoint);
+
+function errorHandler(error, request, response, next) {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformed id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
+  }
+  next(error);
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
